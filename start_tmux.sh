b60a3a37d509df19e412fa5e625a4db2462d3adb
Sh4d0w1337XD <114212240+Sh4d0w1337XD@users.noreply.github.com>
2025-11-07 20:26:21 +0100

    start_tmux.sh

--- start_tmux.sh
+++ start_tmux.sh
@@ -2,7 +2,7 @@
 
 tmux set-option -g default-command "bash -l"
 tmux new-session -d -s root "bash -l"
-tmux split-window -h -t root "node PATH_TO_YOUR_streamers_live.js"
+tmux split-window -h -t root "node PATH_TO_YOUR_streamers.js"
 tmux resize-pane -x 10 -t root:0.1
 tmux set-option -g status-position top
 tmux set-option -g status-right "#[bold]%d-%m-%Y  %H:%M"
